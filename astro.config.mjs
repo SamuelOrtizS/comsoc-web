c721bfab537c526b8df2dcea10b3c23a9cd814e0
SamuelOrtizS <38821308+SamuelOrtizS@users.noreply.github.com>
2026-08-26 21:09:47 -0500

    Agregadas nuevas paginas de error y refactorizadas para ser instancias de ErrorPage.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,5 +22,34 @@ export default defineConfig({
       },
     },
   },
+  redirects: {
+    '/admin': {
+      status: 302,
+      destination: '/403'
+    },
+    '/4o4': {
+      status: 302,
+      destination: '/404'
+    },
+    '/4-o-4': {
+      status: 302,
+      destination: '/404'
+    },
+    '/admin_panel': {
+      status: 302,
+      destination: '/403'
+    },
+    '/secret': {
+      status: 302,
+      destination: '/403'
+    },
+    '/crash': {
+      status: 302,
+      destination: '/500'
+    },
+    '/cicada': {
+      status: 302,
+      destination: '/3301'
+    },
+  }
 });
-
